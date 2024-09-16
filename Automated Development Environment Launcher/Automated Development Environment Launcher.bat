8ca24d2008f5b45fefafd1434ac704a61dc0d932
Sudipta <engineer.sudipta.kumar.das@gmail.com>
2024-09-16 12:48:43 +0600

    Updating

---
+++ Automated Development Environment Launcher/Automated Development Environment Launcher.bat
@@ -0,0 +1,9 @@
+@echo off
+
+start "" "E:\Yaarian\Download_Software_Installation_Folder\Intellij Idea\Intellij Idea Utimate Edition\bin\idea64.exe"
+
+start "" wt -d "C:\Users\User\Desktop\hr__management__system\hr_management_system" powershell -NoExit -Command "npm run dev"
+
+start "" "C:\Users\User\AppData\Local\Postman\Postman.exe"
+
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" "http://localhost:5173/" "https://railway.app/project/f505d486-8607-4236-8ec4-aefc8412629f/service/74a4ea66-77de-4e69-ae21-d939cc2382a4/data" "https://chat.openai.com/"
